0d528f28c2ec93c97517cbd256adecbea89a6687
Kiran Raj Rajendran <krajendran@tesla.com>
2024-10-17 14:06:34 +0200

    adding working

--- MyDetails/source/MyDetails.psm1
+++ MyDetails/source/MyDetails.psm1
@@ -1,25 +1,13 @@
 #region Import Private Functions
-if (Test-Path "$PSScriptRoot\Functions\Private") {
-    $FunctionList = Get-ChildItem -Path "$PSScriptRoot\Functions\Private";
-  
-    foreach ($File in $FunctionList) {
-        . $File.FullName;
-        Write-Verbose -Message ('Importing private function file: {0}' -f $File.FullName);
-    }
+if (Test-Path "$PSScriptRoot\Functions") {
+  $FunctionList = Get-ChildItem -Path "$PSScriptRoot\Functions";
+
+  foreach ($File in $FunctionList) {
+      . $File.FullName;
+      Write-Verbose -Message ('Importing Function: {0}' -f $File.Name);
   }
-  #endregion
-  
-  #region Import Public Functions
-  if (Test-Path "$PSScriptRoot\Functions\Public") {
-    $FunctionList = Get-ChildItem -Path "$PSScriptRoot\Functions\Public";
-  
-    foreach ($File in $FunctionList) {
-        . $File.FullName;
-        Write-Verbose -Message ('Importing public function file: {0}' -f $File.FullName);
-    }
-  }
-  #endregion
-  
-  
-  ### Export all functions
-  Export-ModuleMember -Function *;
+}
+#endregion
+
+### Export all functions - This last line of the file - will be copied by the PSake Build Task
+Export-ModuleMember -Function *;
